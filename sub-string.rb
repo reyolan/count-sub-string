bf0226969734cbcfcf1faa5dd8495d3f73dfc37c
Reynard Mark Olan <reynardolan@gmail.com>
2022-01-14 04:19:33 +0800

    finish substring method

--- sub-string.rb
+++ sub-string.rb
@@ -2,12 +2,11 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 str = "Howdy partner, sit down! How's it going?"
 
 def substrings(string, dictionary)
-  string_occured = dictionary.select {|substring| string.downcase.match?(substring)}
-  number_of_occurence = string_occured.reduce(Hash.new(0)) do |total, substring|
-    total[substring] += 1
-    total
-  end
-  puts number_of_occurence
+  string = string.downcase
+  string_occured = dictionary.select {|substring| string.include?(substring)}
+  number_of_occurence = string_occured.map {|substring| string.scan(substring).length}
+  hash_of_substring_occurence = string_occured.zip(number_of_occurence).to_h  
+  puts hash_of_substring_occurence
 end
   
 substrings(str, dictionary)
